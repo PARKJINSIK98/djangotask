6b01a23b78a6483cb0b800379b3478dcef3b693f
PARKJINSIK98 <127724952+PARKJINSIK98@users.noreply.github.com>
2023-04-08 16:59:19 +0900

    Update urls.py

--- erp/urls.py
+++ erp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('inventory/', views.inventory_show, name='inventory'),
     path('productcreate/', views.product_create, name='product-create'),
     path('inboundcreate/', views.inbound_create, name='inbound-create'),
+    path('outboundcreate/', views.outbound_create, name='outbound-create'),
 ]
